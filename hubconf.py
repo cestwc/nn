20160fc85b3467d09ecb03592b13fac7abbb3db0
cestwc <80936226+cestwc@users.noreply.github.com>
2023-05-04 16:59:00 +0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -9,6 +9,6 @@ from vision.wide_resnet_bn import wide_resnet_8
 def resnet18(pretrained=False, **kwargs):
     model = ResNet18(**kwargs)
     if pretrained:
-        checkpoint = f'https://github.com/cestwc/models/raw/main/pretrained/{pretrained}.pt'
+        checkpoint = f'https://github.com/cestwc/models/raw/pretrained/{pretrained}.pt'
         model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=True))
     return model
